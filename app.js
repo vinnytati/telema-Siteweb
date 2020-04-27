58712cfdc147958572c967981f6d90c8798d6bec
vinnytati <vinnytati00@gmail.com>
2020-04-27 16:22:01 +0100

    creation

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $("#testimol-slider").owlCarousel({
+
+        items : 3,
+        itemsDesktop : [1000,3],
+        itemsDesktopSmall: [979,2],
+        itemsTablet:[768,2],
+        itemsMobile :[650,1],
+        pagination :true,
+        autoPlay:true,
+
+
+
+        <ul class="list-inline">
+                    <li class="list-inline-item"><a href=""><i class="fa fa-facebook"></i></a></li>
+                    <li class="list-inline-item"><a href=""><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href=""><i class="fa fa-github"></i></a></li>
+                  </ul>
+        
+    })
+      
+         
+  });
